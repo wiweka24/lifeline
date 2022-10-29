3cd80c6dbdf253f6e596defde5be25149a591b4a
wiweka24 <yogomu24@gmail.com>
2022-10-29 20:44:34 +0700

    hapus calendar add kalender

---
+++ LifelineNewBuild/Kalender.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Lifeline
+{
+    partial class Kalender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kalender));
+            this.lblMonthAndYear = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.flDays = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnPrevMonth = new System.Windows.Forms.Button();
+            this.btnNextMonth = new System.Windows.Forms.Button();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.btnNewAct = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMonthAndYear
+            // 
+            this.lblMonthAndYear.BackColor = System.Drawing.Color.Transparent;
+            this.lblMonthAndYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMonthAndYear.ForeColor = System.Drawing.Color.White;
+            this.lblMonthAndYear.Location = new System.Drawing.Point(836, 14);
+            this.lblMonthAndYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMonthAndYear.Name = "lblMonthAndYear";
+            this.lblMonthAndYear.Size = new System.Drawing.Size(317, 53);
+            this.lblMonthAndYear.TabIndex = 0;
+            this.lblMonthAndYear.Text = "Januari, 2021";
+            this.lblMonthAndYear.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblMonthAndYear.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(24, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minggu";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(195, 93);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senin";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(365, 93);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 46);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Selasa";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(536, 93);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 46);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Rabu";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(706, 93);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 46);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kamis";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(876, 93);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 46);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Jumat";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(1046, 93);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(163, 46);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Sabtu";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flDays
+            // 
+            this.flDays.BackColor = System.Drawing.Color.Transparent;
+            this.flDays.Location = new System.Drawing.Point(21, 148);
+            this.flDays.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.flDays.Name = "flDays";
+            this.flDays.Size = new System.Drawing.Size(1197, 610);
+            this.flDays.TabIndex = 8;
+            // 
+            // btnPrevMonth
+            // 
+            this.btnPrevMonth.AutoSize = true;
+            this.btnPrevMonth.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrevMonth.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPrevMonth.BackgroundImage")));
+            this.btnPrevMonth.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPrevMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPrevMonth.ForeColor = System.Drawing.Color.Black;
+            this.btnPrevMonth.Location = new System.Drawing.Point(776, 10);
+            this.btnPrevMonth.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnPrevMonth.Name = "btnPrevMonth";
+            this.btnPrevMonth.Size = new System.Drawing.Size(54, 53);
+            this.btnPrevMonth.TabIndex = 9;
+            this.btnPrevMonth.UseVisualStyleBackColor = false;
+            this.btnPrevMonth.Click += new System.EventHandler(this.btnPrevMonth_Click);
+            // 
+            // btnNextMonth
+            // 
+            this.btnNextMonth.AutoSize = true;
+            this.btnNextMonth.BackColor = System.Drawing.Color.Transparent;
+            this.btnNextMonth.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNextMonth.BackgroundImage")));
+            this.btnNextMonth.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNextMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNextMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNextMonth.ForeColor = System.Drawing.Color.Black;
+            this.btnNextMonth.Location = new System.Drawing.Point(1160, 14);
+            this.btnNextMonth.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnNextMonth.Name = "btnNextMonth";
+            this.btnNextMonth.Size = new System.Drawing.Size(54, 53);
+            this.btnNextMonth.TabIndex = 10;
+            this.btnNextMonth.UseVisualStyleBackColor = false;
+            this.btnNextMonth.Click += new System.EventHandler(this.btnNextMonth_Click);
+            // 
+            // btnToday
+            // 
+            this.btnToday.AutoSize = true;
+            this.btnToday.BackColor = System.Drawing.Color.Transparent;
+            this.btnToday.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnToday.BackgroundImage")));
+            this.btnToday.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnToday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToday.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnToday.ForeColor = System.Drawing.Color.Black;
+            this.btnToday.Location = new System.Drawing.Point(75, 10);
+            this.btnToday.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(132, 53);
+            this.btnToday.TabIndex = 11;
+            this.btnToday.UseVisualStyleBackColor = false;
+            this.btnToday.Click += new System.EventHandler(this.btnToday_Click);
+            // 
+            // btnNewAct
+            // 
+            this.btnNewAct.AutoSize = true;
+            this.btnNewAct.BackColor = System.Drawing.Color.Transparent;
+            this.btnNewAct.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNewAct.BackgroundImage")));
+            this.btnNewAct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNewAct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewAct.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNewAct.ForeColor = System.Drawing.Color.Black;
+            this.btnNewAct.Location = new System.Drawing.Point(214, 10);
+            this.btnNewAct.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnNewAct.Name = "btnNewAct";
+            this.btnNewAct.Size = new System.Drawing.Size(180, 53);
+            this.btnNewAct.TabIndex = 12;
+            this.btnNewAct.UseVisualStyleBackColor = false;
+            this.btnNewAct.Click += new System.EventHandler(this.btnNewAct_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(14, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 53);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.AutoSize = true;
+            this.btnRefresh.BackColor = System.Drawing.Color.Transparent;
+            this.btnRefresh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRefresh.BackgroundImage")));
+            this.btnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRefresh.ForeColor = System.Drawing.Color.Black;
+            this.btnRefresh.Location = new System.Drawing.Point(400, 10);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(54, 53);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Kalender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1241, 786);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNewAct);
+            this.Controls.Add(this.btnToday);
+            this.Controls.Add(this.btnNextMonth);
+            this.Controls.Add(this.btnPrevMonth);
+            this.Controls.Add(this.flDays);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMonthAndYear);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Kalender";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalender";
+            this.Load += new System.EventHandler(this.Kalender_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMonthAndYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.FlowLayoutPanel flDays;
+        private System.Windows.Forms.Button btnPrevMonth;
+        private System.Windows.Forms.Button btnNextMonth;
+        private System.Windows.Forms.Button btnToday;
+        private System.Windows.Forms.Button btnNewAct;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
---
+++ LifelineNewBuild/Kalender.cs
@@ -0,0 +1,233 @@
+﻿using Npgsql;
+using System.Configuration;
+using System.Data;
+
+namespace Lifeline
+{
+    public partial class Kalender : Form
+    {
+        private List<FlowLayoutPanel> listFLDays = new List<FlowLayoutPanel>();
+        private List<LinkLabel> listLabel = new List<LinkLabel>();
+        private DataTable activity;
+        private DateTime currentDate = DateTime.Today;
+
+        /// Kalender Load dan beberapa tombol untuk mengakses fungsi
+        public Kalender()
+        {
+            InitializeComponent();
+            var uriString = ConfigurationManager.AppSettings["postgres://szhlblek:O4cczwKuCRX3ta_f_n4K8KjTvvfeSFZW@satao.db.elephantsql.com/szhlblek"] ?? "postgres://postgres:7064@localhost/lifeline";
+            var uri = new Uri(uriString);
+            var db = uri.AbsolutePath.Trim('/');
+            var user = uri.UserInfo.Split(':')[0];
+            var passwd = uri.UserInfo.Split(':')[1];
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                uri.Host, db, user, passwd, port);
+
+            NpgsqlConnection con = new NpgsqlConnection(connStr);
+
+            con.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM activity";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dt = new DataTable();
+                dt.Load(dr);
+            }
+            con.Dispose();
+            con.Close();
+        }
+
+        private void Kalender_Load_1(object sender, EventArgs e)
+        {
+            GenerateDayPanel(42);
+            AddLabelDay(5, 31);
+            DisplayCurrentDate();
+        }
+
+        private void btnPrevMonth_Click(object sender, EventArgs e)
+        {
+            currentDate = currentDate.AddMonths(-1);
+            DisplayCurrentDate();
+        }
+
+        private void btnNextMonth_Click(object sender, EventArgs e)
+        {
+            currentDate = currentDate.AddMonths(1);
+            DisplayCurrentDate();
+        }
+
+        private void btnToday_Click(object sender, EventArgs e)
+        {
+            currentDate = DateTime.Today;
+            DisplayCurrentDate();
+        }
+
+        private void btnNewAct_Click(object sender, EventArgs e)
+        {
+            //ActForm aktBaru = new ActForm();
+            //aktBaru.ShowDialog();
+            //DisplayCurrentDate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCurrentDate();
+        }
+
+        /// Fungsi yang digunakan oleh kode kode diatas
+        private int GetFirstDayOfWeekOfCurrentDate()
+        {
+            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            return (int)(firstDayOfMonth.DayOfWeek + 1);
+        }
+
+        private int GetTotalDaysOfCurrentDate()
+        {
+            DateTime firstDayOfCurrentDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            return firstDayOfCurrentDate.AddMonths(1).AddDays(-1).Day;
+        }
+
+        private void DisplayCurrentDate()
+        {
+            lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
+            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
+            int totalDay = GetTotalDaysOfCurrentDate();
+            AddLabelDay(firstDayAtFlNumber, totalDay);
+            AddAppointmentToFlDay(firstDayAtFlNumber);
+            //AddAppointmentDailyToFlDay(firstDayAtFlNumber);
+        }
+
+        private void AddAppointmentToFlDay(int startDayAtFlNumber)
+        {
+            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            int DayInterval = 1;
+
+            while (startDate <= endDate)
+            {
+                string date = startDate.ToString("dd-MM-yyyy");
+                DataRow[] query = activity.Select("act_date = '" + date + "'");
+                foreach (DataRow item in query)
+                {
+                    DateTime appDay = startDate;
+                    LinkLabel link = new LinkLabel();
+                    link.Text = item[1].ToString();
+                    /*link.Click += new EventHandler(link_Clicked);*/
+                    link.LinkColor = Color.Black;
+                    listLabel.Add(link);
+                    listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
+                }
+
+                if (startDate == DateTime.Today)
+                {
+                    listFLDays[(startDate.Day - 1) + (startDayAtFlNumber - 1)].BackColor = Color.Orange;
+                }
+                startDate = startDate.AddDays(DayInterval);
+            }
+        }
+
+        /*
+        private void AddAppointmentDailyToFlDay(int startDayAtFlNumber)
+        {
+            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            int DayInterval = 1;
+            string[] hari = { "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
+            string hariIni;
+
+            while (startDate <= endDate)
+            {
+                for(int i = 0; i < 7; i++)
+                {
+                    if ((int)startDate.DayOfWeek == i)
+                    {
+                        hariIni = hari[i];
+                        using (var db = new SkheduleDBEntities())
+                        {
+                            var query = from ActDailyTable in db.ActDailyTables where ActDailyTable.waktuDact == hariIni select ActDailyTable;
+                            foreach (var item in query)
+                            {
+                                DateTime appDay = startDate;
+                                LinkLabel link = new LinkLabel();
+                                link.Text = item.namaDact;
+                                link.Click += new EventHandler(link_Clicked);
+                                listLabel.Add(link);
+                                listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
+                            }
+                        }
+                    }
+                }
+                startDate = startDate.AddDays(DayInterval);
+            }
+        }
+
+        private void link_Clicked(object sender, EventArgs e)
+        {
+            LinkLabel link = sender as LinkLabel;
+            link.LinkVisited = true;
+            using (var db = new SkheduleDBEntities())
+            {
+                var query = from ActTable in db.ActTables where ActTable.namaAct == link.Text select ActTable;
+                var query2 = from ActDailyTable in db.ActDailyTables where ActDailyTable.namaDact == link.Text select ActDailyTable;
+                foreach (var item in query)
+                {
+                    ActForm aktivitas = new ActForm(item.namaAct, item.jenisAct, item.waktuAct, item.deskAct);
+                    aktivitas.ShowDialog();
+                }
+                foreach(var item in query2)
+                {
+                    ActForm aktivitas = new ActForm(item.namaDact, item.jenisDact, item.waktuDact, item.deskDact);
+                    aktivitas.ShowDialog();
+                }
+            }
+        }*/
+
+        private void GenerateDayPanel(int totalDays)
+        {
+            flDays.Controls.Clear();
+            listFLDays.Clear();
+            for (int i = 1; i <= totalDays; i++)
+            {
+                FlowLayoutPanel fl = new FlowLayoutPanel();
+                fl.Name = $"flDay{i}";
+                fl.Size = new Size(160, 80);
+                fl.Margin = new Padding(5, 5, 5, 5);
+                fl.BackColor = Color.White;
+                fl.BorderStyle = BorderStyle.FixedSingle;
+                flDays.Controls.Add(fl);
+                listFLDays.Add(fl);
+            }
+        }
+
+        private void AddLabelDay(int StartDay, int totalDayInMonth)
+        {
+            foreach (FlowLayoutPanel fl in listFLDays)
+            {
+                fl.Controls.Clear();
+                fl.BackColor = Color.White;
+            }
+
+            for (int i = 1; i <= totalDayInMonth; i++)
+            {
+                Label lbl = new Label();
+                lbl.Name = $"lblDay{i}";
+                lbl.AutoSize = false;
+                lbl.BackColor = Color.AliceBlue;
+                lbl.TextAlign = ContentAlignment.MiddleRight;
+                lbl.Margin = new Padding(0, 0, 0, -10);
+                lbl.Size = new Size(160, 20);
+                lbl.Text = i.ToString();
+                listFLDays[(i - 1) + (StartDay - 1)].Controls.Add(lbl);
+            }
+        }
+    }
+}
