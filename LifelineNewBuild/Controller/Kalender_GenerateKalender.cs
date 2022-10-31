a7be5be61a7bf5eb553ac10166026f13a9fc79fe
wiweka24 <yogomu24@gmail.com>
2022-10-31 20:27:06 +0700

    memisahkan dalam beberapa file

---
+++ LifelineNewBuild/Controller/Kalender_GenerateKalender.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LifelineNewBuild
+{
+    public partial class Kalender : Form
+    {
+        private List<FlowLayoutPanel> listFLDays = new List<FlowLayoutPanel>();
+        private List<LinkLabel> listLabel = new List<LinkLabel>();
+        private DateTime currentDate = DateTime.Today;
+
+        private int GetFirstDayOfWeekOfCurrentDate()
+        {
+            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            return (int)(firstDayOfMonth.DayOfWeek + 1);
+        }
+
+        private int GetTotalDaysOfCurrentDate()
+        {
+            DateTime firstDayOfCurrentDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            return firstDayOfCurrentDate.AddMonths(1).AddDays(-1).Day;
+        }
+
+        private void DisplayCurrentDate()
+        {
+            lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
+            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
+            int totalDay = GetTotalDaysOfCurrentDate();
+            AddLabelDay(firstDayAtFlNumber, totalDay);
+            AddAppointmentToFlDay(firstDayAtFlNumber);
+        }
+
+        private void AddAppointmentToFlDay(int startDayAtFlNumber)
+        {
+            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            int DayInterval = 1;
+
+            while (startDate <= endDate)
+            {
+                string date = startDate.ToString("dd-MM-yyyy");
+                DataRow[] query = activity.Select("act_date = '" + date + "'");
+                foreach (DataRow item in query)
+                {
+                    DateTime appDay = startDate;
+                    LinkLabel link = new LinkLabel();
+                    link.Text = item[2].ToString();
+                    /*link.Click += new EventHandler(link_Clicked);*/
+                    link.LinkColor = Color.Black;
+                    listLabel.Add(link);
+                    listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
+                }
+
+                if (startDate == DateTime.Today)
+                {
+                    listFLDays[(startDate.Day - 1) + (startDayAtFlNumber - 1)].BackColor = Color.Orange;
+                }
+                startDate = startDate.AddDays(DayInterval);
+            }
+        }
+
+        /*
+        private void AddAppointmentDailyToFlDay(int startDayAtFlNumber)
+        {
+            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            int DayInterval = 1;
+            string[] hari = { "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
+            string hariIni;
+
+            while (startDate <= endDate)
+            {
+                for(int i = 0; i < 7; i++)
+                {
+                    if ((int)startDate.DayOfWeek == i)
+                    {
+                        hariIni = hari[i];
+                        using (var db = new SkheduleDBEntities())
+                        {
+                            var query = from ActDailyTable in db.ActDailyTables where ActDailyTable.waktuDact == hariIni select ActDailyTable;
+                            foreach (var item in query)
+                            {
+                                DateTime appDay = startDate;
+                                LinkLabel link = new LinkLabel();
+                                link.Text = item.namaDact;
+                                link.Click += new EventHandler(link_Clicked);
+                                listLabel.Add(link);
+                                listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
+                            }
+                        }
+                    }
+                }
+                startDate = startDate.AddDays(DayInterval);
+            }
+        }*/
+
+        private void LinkOpenAct()
+        {
+            //LinkLabel link = sender as LinkLabel;
+            //link.LinkVisited = true;
+            //using (var db = new SkheduleDBEntities())
+            //{
+            //    var query = from ActTable in db.ActTables where ActTable.namaAct == link.Text select ActTable;
+            //    var query2 = from ActDailyTable in db.ActDailyTables where ActDailyTable.namaDact == link.Text select ActDailyTable;
+            //    foreach (var item in query)
+            //    {
+            //        ActForm aktivitas = new ActForm(item.namaAct, item.jenisAct, item.waktuAct, item.deskAct);
+            //        aktivitas.ShowDialog();
+            //    }
+            //    foreach(var item in query2)
+            //    {
+            //        ActForm aktivitas = new ActForm(item.namaDact, item.jenisDact, item.waktuDact, item.deskDact);
+            //        aktivitas.ShowDialog();
+            //    }
+            //}
+        }
+
+        private void GenerateDayPanel(int totalDays)
+        {
+            flDays.Controls.Clear();
+            listFLDays.Clear();
+            for (int i = 1; i <= totalDays; i++)
+            {
+                FlowLayoutPanel fl = new FlowLayoutPanel();
+                fl.Name = $"flDay{i}";
+                fl.Size = new Size(136, 75);
+                fl.Margin = new Padding(5, 5, 5, 5);
+                fl.BackColor = Color.White;
+                fl.BorderStyle = BorderStyle.FixedSingle;
+                flDays.Controls.Add(fl);
+                listFLDays.Add(fl);
+            }
+        }
+
+        private void AddLabelDay(int StartDay, int totalDayInMonth)
+        {
+            foreach (FlowLayoutPanel fl in listFLDays)
+            {
+                fl.Controls.Clear();
+                fl.BackColor = Color.White;
+            }
+
+            for (int i = 1; i <= totalDayInMonth; i++)
+            {
+                Label lbl = new Label();
+                lbl.Name = $"lblDay{i}";
+                lbl.AutoSize = false;
+                lbl.BackColor = Color.AliceBlue;
+                lbl.TextAlign = ContentAlignment.MiddleRight;
+                lbl.Margin = new Padding(0, 0, 0, 0);
+                lbl.Size = new Size(136, 20);
+                lbl.Text = i.ToString();
+                listFLDays[(i - 1) + (StartDay - 1)].Controls.Add(lbl);
+            }
+        }
+    }
+}
--- LifelineNewBuild/Kalender.cs
+++ LifelineNewBuild/Kalender.cs
@@ -1,70 +1,20 @@
-﻿using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
+﻿using System;
 using System.Windows.Forms;
 
 namespace LifelineNewBuild
 {
     public partial class Kalender : Form
     {
-        private List<FlowLayoutPanel> listFLDays = new List<FlowLayoutPanel>();
-        private List<LinkLabel> listLabel = new List<LinkLabel>();
-        private DataTable activity;
-        private DateTime currentDate = DateTime.Today;
-        private NpgsqlConnection con;
-        private NpgsqlCommand cmd;
-        private NpgsqlDataReader dr;
-
-        /// Kalender Load dan beberapa tombol untuk mengakses fungsi
         public Kalender()
         {
             InitializeComponent();
-            InitializeConnection();
-            GetActivityTable();
         }
 
-        private void InitializeConnection()
-        {
-            var uriString = "postgres://szhlblek:O4cczwKuCRX3ta_f_n4K8KjTvvfeSFZW@satao.db.elephantsql.com/szhlblek";
-            var uri = new Uri(uriString);
-            var db = uri.AbsolutePath.Trim('/');
-            var user = uri.UserInfo.Split(':')[0];
-            var passwd = uri.UserInfo.Split(':')[1];
-            var port = uri.Port > 0 ? uri.Port : 5432;
-            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
-                uri.Host, db, user, passwd, port);
-
-            con = new NpgsqlConnection(connStr);
-        }
-
-        private void GetActivityTable()
-        {
-            try
-            {
-                con.Open();
-                cmd = new NpgsqlCommand();
-                cmd.Connection = con;
-                cmd.CommandText = "SELECT * FROM activity";
-
-                dr = cmd.ExecuteReader();
-                if (dr.HasRows)
-                {
-                    activity = new DataTable();
-                    activity.Load(dr);
-                }
-                con.Dispose();
-                con.Close();
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show("Error: " + err.Message, "FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
+        // Awal Load Calender
         private void Kalender_Load(object sender, EventArgs e)
         {
+            InitializeConnection();
+            GetActivityTable();
             GenerateDayPanel(42);
             AddLabelDay(5, 31);
             DisplayCurrentDate();
@@ -88,16 +38,18 @@ namespace LifelineNewBuild
             DisplayCurrentDate();
         }
 
+        // Klik Aktivitas Baru -> Buka Form Aktivitas
         private void btnNewAct_Click(object sender, EventArgs e)
         {
-            ActForm aktBaru = new ActForm();
-            aktBaru.ShowDialog();
-            //DisplayCurrentDate();
+            ActForm newAct = new ActForm();
+            newAct.ShowDialog();
+            DisplayCurrentDate();
         }
 
-        private void btnExit_Click(object sender, EventArgs e)
+        // Klik Link di Aktivitas
+        private void link_Clicked(object sender, EventArgs e)
         {
-            Application.Exit();
+            LinkOpenAct();
         }
 
         private void btnRefresh_Click(object sender, EventArgs e)
@@ -105,149 +57,9 @@ namespace LifelineNewBuild
             DisplayCurrentDate();
         }
 
-        private int GetFirstDayOfWeekOfCurrentDate()
+        private void btnExit_Click(object sender, EventArgs e)
         {
-            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
-            return (int)(firstDayOfMonth.DayOfWeek + 1);
-        }
-
-        private int GetTotalDaysOfCurrentDate()
-        {
-            DateTime firstDayOfCurrentDate = new DateTime(currentDate.Year, currentDate.Month, 1);
-            return firstDayOfCurrentDate.AddMonths(1).AddDays(-1).Day;
-        }
-
-        private void DisplayCurrentDate()
-        {
-            lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
-            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
-            int totalDay = GetTotalDaysOfCurrentDate();
-            AddLabelDay(firstDayAtFlNumber, totalDay);
-            AddAppointmentToFlDay(firstDayAtFlNumber);
-        }
-
-        private void AddAppointmentToFlDay(int startDayAtFlNumber)
-        {
-            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
-            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
-            int DayInterval = 1;
-
-            while (startDate <= endDate)
-            {
-                string date = startDate.ToString("dd-MM-yyyy");
-                DataRow[] query = activity.Select("act_date = '" + date + "'");
-                foreach (DataRow item in query)
-                {
-                    DateTime appDay = startDate;
-                    LinkLabel link = new LinkLabel();
-                    link.Text = item[2].ToString();
-                    /*link.Click += new EventHandler(link_Clicked);*/
-                    link.LinkColor = Color.Black;
-                    listLabel.Add(link);
-                    listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
-                }
-
-                if (startDate == DateTime.Today)
-                {
-                    listFLDays[(startDate.Day - 1) + (startDayAtFlNumber - 1)].BackColor = Color.Orange;
-                }
-                startDate = startDate.AddDays(DayInterval);
-            }
-        }
-
-        /*
-        private void AddAppointmentDailyToFlDay(int startDayAtFlNumber)
-        {
-            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
-            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
-            int DayInterval = 1;
-            string[] hari = { "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
-            string hariIni;
-
-            while (startDate <= endDate)
-            {
-                for(int i = 0; i < 7; i++)
-                {
-                    if ((int)startDate.DayOfWeek == i)
-                    {
-                        hariIni = hari[i];
-                        using (var db = new SkheduleDBEntities())
-                        {
-                            var query = from ActDailyTable in db.ActDailyTables where ActDailyTable.waktuDact == hariIni select ActDailyTable;
-                            foreach (var item in query)
-                            {
-                                DateTime appDay = startDate;
-                                LinkLabel link = new LinkLabel();
-                                link.Text = item.namaDact;
-                                link.Click += new EventHandler(link_Clicked);
-                                listLabel.Add(link);
-                                listFLDays[(appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
-                            }
-                        }
-                    }
-                }
-                startDate = startDate.AddDays(DayInterval);
-            }
-        }
-
-        private void link_Clicked(object sender, EventArgs e)
-        {
-            LinkLabel link = sender as LinkLabel;
-            link.LinkVisited = true;
-            using (var db = new SkheduleDBEntities())
-            {
-                var query = from ActTable in db.ActTables where ActTable.namaAct == link.Text select ActTable;
-                var query2 = from ActDailyTable in db.ActDailyTables where ActDailyTable.namaDact == link.Text select ActDailyTable;
-                foreach (var item in query)
-                {
-                    ActForm aktivitas = new ActForm(item.namaAct, item.jenisAct, item.waktuAct, item.deskAct);
-                    aktivitas.ShowDialog();
-                }
-                foreach(var item in query2)
-                {
-                    ActForm aktivitas = new ActForm(item.namaDact, item.jenisDact, item.waktuDact, item.deskDact);
-                    aktivitas.ShowDialog();
-                }
-            }
-        }*/
-
-        private void GenerateDayPanel(int totalDays)
-        {
-            flDays.Controls.Clear();
-            listFLDays.Clear();
-            for (int i = 1; i <= totalDays; i++)
-            {
-                FlowLayoutPanel fl = new FlowLayoutPanel();
-                fl.Name = $"flDay{i}";
-                fl.Size = new Size(136, 75);
-                fl.Margin = new Padding(5, 5, 5, 5);
-                fl.BackColor = Color.White;
-                fl.BorderStyle = BorderStyle.FixedSingle;
-                flDays.Controls.Add(fl);
-                listFLDays.Add(fl);
-            }
-        }
-
-        private void AddLabelDay(int StartDay, int totalDayInMonth)
-        {
-            foreach (FlowLayoutPanel fl in listFLDays)
-            {
-                fl.Controls.Clear();
-                fl.BackColor = Color.White;
-            }
-
-            for (int i = 1; i <= totalDayInMonth; i++)
-            {
-                Label lbl = new Label();
-                lbl.Name = $"lblDay{i}";
-                lbl.AutoSize = false;
-                lbl.BackColor = Color.AliceBlue;
-                lbl.TextAlign = ContentAlignment.MiddleRight;
-                lbl.Margin = new Padding(0, 0, 0, 0);
-                lbl.Size = new Size(133, 20);
-                lbl.Text = i.ToString();
-                listFLDays[(i - 1) + (StartDay - 1)].Controls.Add(lbl);
-            }
+            Application.Exit();
         }
     }
 }
